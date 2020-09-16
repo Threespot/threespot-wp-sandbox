c531f911af7bf622eaa36e540213267d5b804fdb
Henry Mesias <henrymesias@gmail.com>
2020-09-15 22:22:33 -0400

    feat: added twenty twenty child theme and inner blocks test

---
+++ web/wp-content/themes/twentytwenty-child/template-parts/block-team-member.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Team Member block
+ * https://www.billerickson.net/building-gutenberg-block-acf/
+ * @package      ClientName
+ * @author       Bill Erickson
+ * @since        1.0.0
+ * @license      GPL-2.0+
+**/
+
+$name = get_field( 'name' );
+$title = get_field( 'title' );
+$photo = get_field( 'photo' );
+$description = get_field( 'description' );
+
+
+echo '<div class="team-member">';
+	echo '<div class="team-member--header">';
+		if( !empty( $photo ) )
+			echo wp_get_attachment_image( $photo['ID'], 'thumbnail', null, array( 'class' => 'team-member--avatar' ) );
+		if( !empty( $name ) )
+			echo '<h4>' . esc_html( $name ) . '</h4>';
+		if( !empty( $title ) )
+			echo '<h6 class="alt">' . esc_html( $title ) . '</h6>';
+	echo '</div>';
+    echo '<div class="team-member--content">' . apply_filters( 'ea_the_content', $description ) . '</div>';
+    echo '<div class="team-member--innerBlocks">';
+    echo '<InnerBlocks />';
+    echo '</div>';
+echo '</div>';
+
