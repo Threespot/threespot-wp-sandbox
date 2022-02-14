25e136092fc62d3327be2efeefb3a0c6d069b3e6
Ted Whitehead <tedwhitehead@gmail.com>
2022-02-14 15:35:11 -0500

    feat: remove custom and themes for WETA pitch, update build script

--- web/wp-content/themes/threespot-wp-sandbox/resources/assets/scripts/modules/background-picture.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import ObjectFitImage from "@threespot/object-fit-image";
-
-var imageWrappers = document.querySelectorAll(".bg-image");
-
-imageWrappers.forEach(el => new ObjectFitImage(el, {
-  visuallyHiddenClass: "u-screenreader"
-}));
