aa19a6bbcda0338627b709e1c5fa43e47d587d20
Chris Heuberger <chrisheuberg@gmail.com>
2025-06-09 18:19:26 -0400

    test: trigger re-deploy

--- web/wp-content/themes/threespot-wp-sandbox/resources/views/layouts/app.blade.php
+++ web/wp-content/themes/threespot-wp-sandbox/resources/views/layouts/app.blade.php
@@ -4,11 +4,13 @@
   <body @php body_class() @endphp>
     @php do_action('get_header') @endphp
     @include('partials.header')
+
     <div class="wrap container" role="document">
       <div class="content">
         <main class="main">
           @yield('content')
         </main>
+
         @if (App\display_sidebar())
           <aside class="sidebar">
             @include('partials.sidebar')
@@ -16,6 +18,7 @@
         @endif
       </div>
     </div>
+
     @php do_action('get_footer') @endphp
     @include('partials.footer')
     @php wp_footer() @endphp
