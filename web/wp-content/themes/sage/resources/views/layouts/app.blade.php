9b5b6f8516da3664c8eda2b0bdd608cc6db365c8
Ted Whitehead <tedwhitehead@gmail.com>
2025-08-25 14:56:10 -0400

    feat: update app template, remove old svg sprite

--- web/wp-content/themes/sage/resources/views/layouts/app.blade.php
+++ web/wp-content/themes/sage/resources/views/layouts/app.blade.php
@@ -1,15 +1,16 @@
 <!doctype html>
-<html class="no-js" {!! get_language_attributes() !!}>
+<html class="no-js smooth-scroll" {!! get_language_attributes() !!}>
   @include('partials.head')
-  <body @php body_class() @endphp>
-    @include('partials.svg-sprite')
+  <body @php(body_class())>
+    @php(wp_body_open())
     {{-- Google Tag Manager (noscript) --}}
     @if ($is_production && !empty($gtm_id))
       <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ $gtm_id }}"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     @endif
-    @php do_action('get_header') @endphp
+    @php(do_action('get_header'))
     @include('partials.skip-links')
+    {{-- Header --}}
     @include('partials.header')
     <main class="Main" id="main">
       {{-- Login form for password-protected pages --}}
@@ -23,38 +24,42 @@
         @yield('content')
       @endif
     </main>
+    {{-- Footer --}}
     @php(do_action('get_footer'))
     @include('partials.footer')
     @php(wp_footer())
-
     {{--
-      FIXME: This code requires an “adobe_fonts_url” ACF text field in Theme Settings
       Optional: Adobe Fonts tracking code (formerly Typekit)
       Since we’re hosting the fonts locally, we need to manually request their tracking file.
-      - The tracking file URL is from the official CSS file (https://use.typekit.net/fcu1equ.css)
+      - The tracking file URL is from the official CSS file (https://use.typekit.net/xgt4mvl.css)
       - AJAX request JS from https://plainjs.com/javascript/ajax/making-cors-ajax-get-requests-54/
       - JS was minified using https://skalman.github.io/UglifyJS-online/
 
       Below is the un-minified code:
 
-        function getCORS(url, success, failure) {
-          var xhr = new XMLHttpRequest();
-          if (!('withCredentials' in xhr)) xhr = new XDomainRequest(); // fix IE8/9
-          xhr.open('GET', url);
-          xhr.onload = success;
-          xhr.addEventListener('error', failure);
-          xhr.send();
-          return xhr;
-        }
+      async function fetchCORS(url) {
+        try {
+          const response = await fetch(url, {
+            method: 'GET',
+            mode: 'cors'
+          });
 
-        getCORS('{{ $adobe_fonts_url }}', function(request) {
+          if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+          }
+
           // Success, do nothing
-        }, function(error) {
+          return response;
+        } catch (error) {
           console.warn('Could not ping Adobe Fonts tracking URL');
-        });
+          throw error;
+        }
+      }
+
+      fetchCORS('{{ $adobe_fonts_url }}');
     --}}
     {{-- @if ($is_production && !empty($adobe_fonts_url))
-      <script>function getCORS(n,e,t){var o=new XMLHttpRequest;return(o="withCredentials"in o?o:new XDomainRequest).open("GET",n),o.onload=e,o.addEventListener("error",t),o.send(),o}getCORS("{{ $adobe_fonts_url }}",function(n){},function(n){console.warn("Could not ping Adobe Fonts tracking URL")});</script>
+      <script>async function fetchCORS(t){try{var o=await fetch(t,{method:"GET",mode:"cors"});if(o.ok)return o;throw new Error("Response status: "+o.status)}catch(t){throw console.warn("Could not ping Adobe Fonts tracking URL"),t}}fetchCORS("{{ $adobe_fonts_url }}");</script>
     @endif --}}
   </body>
 </html>
